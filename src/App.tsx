17ed7c2cae5d52e8c3aa188f8221d8fd82594c32
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-12-12 21:06:51 -0600

    Move account settings page to new directory structure and update import path

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import './App.css'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
-import AccountSettingsPage from './Pages/Auth/settings/account'
+import AccountSettingsPage from './Pages/settings/account'
 
 function App() {
   const [count, setCount] = useState(0)
